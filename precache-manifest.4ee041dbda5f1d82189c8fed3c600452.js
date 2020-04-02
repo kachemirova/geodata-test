cae64684d70e7df05c29cc76f8559dfed12ef21c
kachemirova <kachemirovaelena@gmail.com>
2020-04-02 17:03:44 +0300

    Updates

---
+++ precache-manifest.4ee041dbda5f1d82189c8fed3c600452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c166ab62d5f90017625ec105bbbb910",
+    "url": "/geodata-test/index.html"
+  },
+  {
+    "revision": "c4bef255c3726e5eeeec",
+    "url": "/geodata-test/static/css/main.4979cf63.chunk.css"
+  },
+  {
+    "revision": "ad2369e9dd369c19b3ec",
+    "url": "/geodata-test/static/js/2.cb90b41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/geodata-test/static/js/2.cb90b41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bef255c3726e5eeeec",
+    "url": "/geodata-test/static/js/main.866aeef8.chunk.js"
+  },
+  {
+    "revision": "074593c57595e7c6efb2",
+    "url": "/geodata-test/static/js/runtime-main.4ee812b5.js"
+  }
+]);
